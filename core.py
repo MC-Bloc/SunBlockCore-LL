00574eaca77a46d9b161848844e118bccc4f9afa
Muhammad Shahrom Ali <a_madsh@live.concordia.ca>
2025-05-29 23:43:11 -0400

    Read from RS45 Cable directly

--- core.py
+++ core.py
@@ -1,16 +1,14 @@
 from dotenv import load_dotenv
-import serial
-import time
-import re
 import json
 import subprocess
 from datetime import datetime
 import os
 from enum import Enum
 import sqlite3
+from epevermodbus.driver import EpeverChargeController
 
 
-USBDEV = "/dev/ttyUSB0"
+CONTROLLER = EpeverChargeController("/dev/ttyACM0", 1)
 DATA_DIRECTORY = "/home/pc/SunblockData/"
 POWER_DRAW_SCRIPT_ADDR = "/home/pc/power_scripts/powerdraw.sh"
 ACTIVE_DATA = DATA_DIRECTORY + "solar_data.json"
@@ -71,88 +69,64 @@ def WriteDB():
         DB_CURSOR.execute(db_query)
         DB_CONNECTION.commit()
 
-def ParseData(data_string):
-    global JSON_DATA
 
-    data_array = data_string.strip().split(" ")
 def CheckPowerProfile():
     return os.popen("sudo powerprofilesctl get").read().strip()
 
 
+def ParseData():
+    global JSON_DATA
+
     JSON_DATA["Timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    JSON_DATA["PVVoltage"] = data_array[0].strip()
-    JSON_DATA["PVCurrent"] = data_array[1].strip()
-    JSON_DATA["PVPower"] = data_array[2].strip()
+    JSON_DATA["PVVoltage"] = CONTROLLER.get_solar_voltage()
+    JSON_DATA["PVCurrent"] = CONTROLLER.get_solar_current()
+    JSON_DATA["PVPower"] = CONTROLLER.get_solar_power()
 
-    JSON_DATA["BattVoltage"] = data_array[3].strip()
-    JSON_DATA["BattChargeCurrent"] = data_array[4].strip()
-    JSON_DATA["BattChargePower"] = data_array[5].strip()
-    JSON_DATA["BattOverallCurrent"] = data_array[9].strip()
-    JSON_DATA["BattPercentage"] = data_array[7].strip()
+    JSON_DATA["BattVoltage"] = CONTROLLER.get_battery_voltage()
+    JSON_DATA["BattTemperature"] = CONTROLLER.get_battery_temperature()
+    JSON_DATA["BattChargePower"] = CONTROLLER.get_battery_power()
+    JSON_DATA["BattOverallCurrent"] = CONTROLLER.get_battery_current()
+    JSON_DATA["BattPercentage"] = CONTROLLER.get_battery_state_of_charge()
 
-    JSON_DATA["LoadPower"] = data_array[6].strip()
+    JSON_DATA["LoadPower"] = CONTROLLER.get_load_power()
 
     powerdraw = subprocess.run(POWER_DRAW_SCRIPT_ADDR, capture_output=True)
     powerdraw = powerdraw.stdout.decode().replace("W", "").strip()
     JSON_DATA["CPUPowerDraw"] = powerdraw
-    
-    JSON_DATA["PowerProfile"] = os.popen("sudo powerprofilesctl get").read().strip()
+    JSON_DATA["PowerProfile"] = CheckPowerProfile()
 
 
-# POWER MANAGEMENT
-def Powerlog(log):
+def SunBlockLog(log):
     with open(POWER_LOGS_FILE, 'a') as log_file:
         log_file.write(datetime.now().strftime(
             "%Y-%m-%d %H:%M:%S") + ": " + log + "\n")
 
 
 def Main():
-    first_time = True
+    SunBlockLog("Waking Up...")
+
     load_dotenv()
-    Powerlog("\nWaking Up...")
-    Powerlog("Power Management is " + str(POWER_MAN))
-    Powerlog("Data Management is " + str(DATA_MAN))
 
-    print("Starting serial reader from: ", USBDEV)
-    SerialObj = serial.Serial(USBDEV, 115200)
-    time.sleep(3)  # MSA: Why is this done?
-    SerialObj.timeout = 3  # read timeout
-    CheckDB()
+    if DATA_MAN:
+        SunBlockLog("Data Management is " + str(DATA_MAN))
+        CheckDB()
 
-    GetAMPInstance()
-
-    while (SerialObj.is_open):
+    while (CONTROLLER != None):
         try:
-            ReceivedString = SerialObj.readline().decode("utf-8")
-            matches = re.findall("(\d)+\.\d\d", ReceivedString)
-            if (matches and len(matches) == 10):
-
-                ParseData(ReceivedString)
-
-                WriteJSON()
-                if DATA_MAN:
-                    WriteDB()
-
-                if POWER_MAN:
-                    ProfileAndStateValidation()
-                    if first_time:
-                        Powerlog(str(JSON_DATA))
-                        Powerlog("State: " + str(CURRENT_STATE) +
-                                 ", Power Saving: " + str(POWER_SAVER_ON))
-                        CheckFailsafes()
-                        first_time = False
-
-                    PowerStateManagement()
-                    PowerProfileManagement()
+            ParseData()
+            WriteJSON()
+            
+            if DATA_MAN:
+                WriteDB()
 
         except Exception or KeyboardInterrupt:
-            Powerlog("Error: Couldnt continue data logging. \n\nExiting...")
-            SerialObj.close()
+            SunBlockLog(
+                "Error: Couldnt continue data logging. \nExiting...\n\n")
             if DB_CONNECTION:
                DB_CONNECTION.close()
             return
-    Powerlog("Exiting While loop. Serial Object Closed.")
+    SunBlockLog("Exiting While loop. Serial Object Closed.")
 
 
 Main()
